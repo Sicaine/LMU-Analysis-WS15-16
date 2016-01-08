4c6f2d077e28e704c78999e9160dff29db35b229
Siegfried Kiermayer <sicaine@gmail.com>
2016-01-08 14:51:11 +0100

    add bootstrap and basic content to index.php
    now you are able to use bootstrap and have the downloadlink.

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Downloadpage for LMU Analysis Script</title>
+	<meta charset="UTF-8">
+	<title>Downloadpage for LMU Analysis Script</title>
+
+	<!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-<?PHP echo 'blub'; ?>
+	<h1>Analysis Script Status Page</h1>
 
+	Aktuelle Skriptversion <a href="https://lmu-analysis-ws15-16.herokuapp.com/helloworld.pdf">herunterladen</a>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
